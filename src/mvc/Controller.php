462a7e07801a01234d6089d929c445c2325bad66
sam <sam@imccc.local>
2024-03-05 23:31:18 +0800

    fixed error

--- src/mvc/Controller.php
+++ src/mvc/Controller.php
@@ -10,24 +10,28 @@ class Controller
         $this->routes = $routes;
     }
 
-    //
-    public function input($ps = '')
+    /**
+     * 读取参数
+     *
+     * @param string $ps 参数键名（使用点分隔表示嵌套关系）
+     * @return mixed 如果提供了参数，则返回对应的值，否则返回整个 $this->routes 数组
+     */
+    public function input(string $ps = ''): mixed
     {
         $alldata = $this->routes;
+
+        // 检查是否提供了参数
         if (empty($ps) || !isset($ps)) {
             return $alldata;
         } else {
             $pm = explode('.', $ps);
             foreach ($pm as $val) {
+                // 逐级深入数组
                 if (isset($alldata[$val])) {
-                    $alldata = $alldata[$val];
+                    return $alldata[$val];
                 }
             }
-            return $alldata;
-
         }
-
-        return $this;
     }
 
 }
