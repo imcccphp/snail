5ec33d07ab147f76612b52fc2e9eed3a68d725a6
sam <sam@imccc.local>
2024-03-05 20:44:24 +0800

    fixed error

---
+++ src/mvc/Controller.php
@@ -0,0 +1,20 @@
+<?php
+namespace Imccc\Snail\Mvc;
+
+class Controller
+{
+    protected $routes;
+    public function __construct($routes)
+    {
+        $this->routes = $routes;
+    }
+
+    //
+    public function input()
+    {
+
+        print_r($this->routes);
+
+        return $this;
+    }
+}
